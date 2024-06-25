9bc4d3013b01fbfb610bd84247a662d8d057239a
Kirill Gorbunov <4pduck@gmail.com>
2024-06-25 03:26:33 +0300

    fixed commit

--- Homework STL 1.1/Homework STL 1.1.cpp
+++ Homework STL 1.1/Homework STL 1.1.cpp
@@ -1,13 +1,15 @@
 ﻿#include <iostream>
 #include <map>
-#include<string>
 #include <cctype>  
+#include <vector>  
+#include <algorithm>
+#include<string>
 
 std::map<char, int> count_characters(const std::string& text) {
     std::map<char, int> char_count;
 
     for (char ch : text) {
-        
+
         char lower_ch = std::tolower(static_cast<unsigned char>(ch));
 
         if (std::isalnum(lower_ch)) {
@@ -18,26 +20,26 @@ std::map<char, int> count_characters(const std::string& text) {
     return char_count;
 }
 
-void print_character_count(const std::map<char, int>& char_count) {
-    for (const auto& pair : char_count) {
-        std::cout << pair.first << ": " << pair.second << std::endl;
-    }
-}
-
 int main() {
     std::string text;
     std::cout << "Enter a text: ";
 
     std::cin >> std::ws;
-
- 
     std::getline(std::cin, text);
-
 
     std::map<char, int> char_count = count_characters(text);
 
+    std::vector<std::pair<char, int>> char_vector(char_count.begin(), char_count.end());
+
+    std::sort(char_vector.begin(), char_vector.end(),
+        [](const auto& left, const auto& right) {
+            return left.second > right.second;
+        });
+
     std::cout << "Character frequencies (descending order):" << std::endl;
-    print_character_count(char_count);
+    for (const auto& pair : char_vector) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
 
     return 0;
 }
